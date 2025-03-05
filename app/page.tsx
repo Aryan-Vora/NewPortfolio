d668a515da661f093b0707f79ad00ddf3e47886e
Aryan Vora <voraaryan1@gmail.com>
2025-03-04 19:59:41 -0800

    used a ref

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,36 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import { useEffect } from 'react';
-export default function Home() {
+import { useEffect, useRef, RefObject } from 'react';
+
+interface HomeProps {
+  index: number;
+  selected: number;
+}
+
+export default function Home(props: HomeProps) {
+  const childDiv: RefObject<HTMLDivElement> = useRef(null);
+
+  const handleScroll = () => {
+    const { index, selected } = props;
+    if (index === selected) {
+      setTimeout(() => {
+        if (childDiv.current) {
+          childDiv.current.scrollIntoView();
+        }
+      }, 100);
+    }
+  };
+
   useEffect(() => {
-    setTimeout(() => {
-      window.scrollTo(0, 0);
-    });
-  }, []);
+    handleScroll();
+  }, [props.index, props.selected]);
+
   return (
     <>
-      <Hero />
+      <div ref={childDiv}>
+        <Hero />
+      </div>
       <About />
       <Skills />
       <Projects />
