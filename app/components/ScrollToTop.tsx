fc2e7d7001de73de1018b8c9af4d040e026024e8
Aryan Vora <voraaryan1@gmail.com>
2025-03-04 19:27:22 -0800

    Ok

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -11,6 +11,17 @@ const ScrollToTop = ({ children }: ScrollToTopProps) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const isFirstRender = useRef(true);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (history.scrollRestoration) {
+        history.scrollRestoration = 'manual';
+      } else {
+        window.onbeforeunload = function () {
+          window.scrollTo(0, 0);
+        };
+      }
+    }
+  }, []);
 
   useEffect(() => {
     if (isFirstRender.current) {
